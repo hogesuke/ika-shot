8c8c542fb52486eee4d1784b7fc6ccff561a600d
hogesuke <miyado@gmail.com>
2015-07-26 07:09:59 +0900

    Update unicorn.rb

--- unicorn.rb
+++ unicorn.rb
@@ -5,7 +5,7 @@ working_directory @dir
 
 timeout 300
 # listen 4567
-listen "#{@dir}tmp/unicorn.sock", backlog: 1024 # UNIXソケットを見るように変更
+listen "/tmp/unicorn.sock", backlog: 1024 # UNIXソケットを見るように変更
 
 pid "#{@dir}tmp/pids/unicorn.pid"
 
